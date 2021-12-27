3a86517167ed64bcaef15ecb891d775eb8d76832
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2021-12-27 13:38:36 +0100

    Removes version pin on AWS provider
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
     }
   }
+
   required_version = ">= 0.13"
 }
